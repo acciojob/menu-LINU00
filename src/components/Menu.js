4f78e42122fcfa445264c098bb381928ba488118
Aditya Singh <103933450+LINU00@users.noreply.github.com>
2025-11-06 01:56:22 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,23 @@
 import React from "react";
 
-function Menu({ items }) {
+const Menu = ({ dishes }) => {
   return (
-    <div className="menu-container">
-      {items.map((dish) => {
-        
-        let testId = "";
-        if (dish.category === "Breakfast") testId = "menu-item-breakfast";
-        else if (dish.category === "Lunch") testId = "menu-item-lunch";
-        else if (dish.category === "Shakes") testId = "menu-item-shakes";
-
-        return (
-          <div key={dish.id} className="menu-item" data-test-id={testId}>
-            <img src={dish.img} alt={dish.name} className="dish-img" />
+    <div className="menu-list">
+      {dishes.map((dish) => (
+        <div
+          key={dish.id}
+          className="menu-item"
+          data-test-id={`menu-item-${dish.category}`}
+        >
+          <img src={dish.img} alt={dish.name} className="menu-img" />
+          <div className="menu-info">
             <h3>{dish.name}</h3>
-            <p>{dish.category}</p>
             <p>{dish.price}</p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Menu;
